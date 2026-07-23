a27fb5a4da73ba47dc184627da43b7e90bb14bbe
wanderer <far.jolani@gmial.com>
2026-07-23 12:25:13 +0330

    feat(core): solve Chiraxi exersice

--- week-03/1_Chiraxi/main.py
+++ week-03/1_Chiraxi/main.py
@@ -1,6 +1,26 @@
 class ExceptionProxy(Exception):
-    ...
+    def __init__(self, message, function):
+        self.message = message
+        self.function = function
+
+
 
 
 def transform_exceptions(func_ls: list) -> list[ExceptionProxy]:
-    ...
+    result = []
+    for func in func_ls:
+        try:
+            func()
+            err = ExceptionProxy("ok!", func)
+            result.append(err)
+
+
+
+        except Exception as e:
+            err = ExceptionProxy(str(e), func)
+            result.append(err)
+
+    return result
+
+
+
